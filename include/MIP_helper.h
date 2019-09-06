9bedcf79dee7190d2a02921861dcac19293c781c
Alexey R <romanyuta@netpost.ru>
2019-09-06 20:17:40 +0700

    Added preset functions

--- include/MIP_helper.h
+++ include/MIP_helper.h
@@ -1,21 +1,19 @@
-#ifndef MIP_DECODER_H
-#define MIP_DECODER_H
+#ifndef MIP_HELPER_H
+#define MIP_HELPER_H
 /*Include Headers-------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /*Include user-defined headers------------------------------------------------*/
-#include <MIP_helper.h>
 
 /*Constants-------------------------------------------------------------------*/
+#define NOTHING                                                                \
+  {}
 
 /*Declarations----------------------------------------------------------------*/
 
 /*Functions-------------------------------------------------------------------*/
-int decoder(int code);
-int fi0_decoder();
-int fi1_decoder();
-int fi2_decoder();
+
 /*----------------------------------------------------------------------------*/
 #endif
--- src/main.c
+++ src/main.c
@@ -10,13 +10,43 @@
 
 /*Functios--------------------------------------------------------------------*/
 int main(int argc, char **argv) {
-  helper();
-  return 0;
+  char *method = argv[1];
+  char *code = argv[2];
+  int exitcode = -1;
+  if (argc > 1) {
+    if (!strcmp(method, "coder")) {
+      /*Coder*/
+      if (code == NULL) {
+        fprintf(stderr, "Parameter not found! [coder]\n");
+        return helper();
+      }
+      if ((exitcode = coder(atoi(code))) == 255) {
+        fprintf(stderr, "Code invalid [coder]\n");
+        return helper();
+      } else {
+        return exitcode;
+      }
+    } else if (!strcmp(method, "decoder")) {
+      /*Decoder*/
+      if (code == NULL) {
+        fprintf(stderr, "Parameter not found! [decoder]\n");
+        return helper();
+      }
+      if ((exitcode = decoder(atoi(code))) == 255) {
+        fprintf(stderr, "Code invalid [decoder]\n");
+        return helper();
+      } else {
+        return exitcode;
+      }
+    }
+  }
+  return helper();
 }
 /*----------------------------------------------------------------------------*/
-void helper() {
-  printf("Usage:\n");
-  printf("  \n");
-  printf("  \n");
+int helper() {
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "  mip coder\n");
+  fprintf(stderr, "  mip decoder\n");
+  return 0;
 }
 /*----------------------------------------------------------------------------*/

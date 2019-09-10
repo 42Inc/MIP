850d3d2de17c3f50ef561b7220686a346578251e
Alexey R <romanyuta@netpost.ru>
2019-09-10 09:18:22 +0700

    Decoder. Function 1

--- src/decoder.c
+++ src/decoder.c
@@ -26,6 +26,34 @@ int decoder(int code) {
 }
 /*----------------------------------------------------------------------------*/
 int fi0_decoder() {
+  char c = 0;
+  int return_code = 0;
+  int write_num = 0;
+  char *out_filename = "a.decode";
+  FILE *out_fd = NULL;
+  int i = 0;
+  if ((out_fd = fopen(out_filename, "w")) == NULL) {
+    fprintf(stderr, "File open error! [decoder]\n");
+    return 1;
+  }
+  do {
+    return_code = read(STDIN_FILENO, &c, 1);
+    if (c == 0) {
+      write_num += 8;
+    } else {
+      for (i = 0; i < 8; ++i) {
+        if ((c & 0x80) == 0) {
+          write_num++;
+        } else {
+          fprintf(stderr, "Write num - %d [decoder]\n", write_num);
+          fprintf(out_fd, "%d ", write_num);
+          write_num = 0;
+        }
+        c = c << 1;
+      }
+    }
+  } while (return_code != 0);
+  // write last byte
   return 0;
 }
 /*----------------------------------------------------------------------------*/

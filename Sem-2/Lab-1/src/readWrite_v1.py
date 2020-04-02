2051c3d2c2533d710d8d7e6bfcf7f25a52405f32
Doshchinsky <akter1997@gmail.com>
2020-04-02 19:27:41 +0700

    Finished second version of python prog

--- Sem-2/Lab-1/src/readWrite_v1.py
+++ Sem-2/Lab-1/src/readWrite_v1.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python3
 
 import random
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-max = 26
-for a in range(1, 50):
-	c1_mid = 0
-	c2_mid = 0
-	for i in range(1, 1000):
-		c1 = 0
-		c2 = 0
-		rand = 1
-		while(rand != 0):
-			rand = random.randint(0, 100)
-			if(1 <= rand < a and c1+1 <= max):
-				c1 += 1
-			elif(a <= rand < (2*a) and c2+1 <= max):
-				c2 += 1
+limit = 26
+plt_x_vec = [cnt for cnt in range(50)]
+stream_1_result = [0 for i in range(50)]
+stream_2_result = [0 for j in range(50)]
+
+for alfa in range(1, 51):
+	stream_1_mean = 0
+	stream_2_mean = 0
+
+	for i in range(1, 1001):
+		stream_1 = 0
+		stream_2 = 0
+		prob = 1
+
+		while (prob != 0):
+			prob = random.randint(0, 100)
+
+			if (1 <= prob < alfa and stream_1 + 1 <= limit):
+				stream_1 += 1
+			elif (alfa <= prob < (2 * alfa) and stream_2 + 1 <= limit):
+				stream_2 += 1
 			else:
 				continue
-		c1_mid += c1
-		c2_mid += c2
-	print(c1_mid/1000,'\t', c2_mid/1000)
+
+		stream_1_mean += stream_1
+		stream_2_mean += stream_2
+
+	stream_1_result[alfa-1] = stream_1_mean/1000
+	stream_2_result[alfa-1] = stream_2_mean/1000
+
+plt.plot(plt_x_vec, stream_1_result, label="Первый поток чтения")
+plt.plot(plt_x_vec, stream_2_result, label="Второй поток чтения")
+plt.ylabel("Среднее кол-во считанных символов")
+plt.xlabel("Массив символов")
+plt.legend()
+plt.show()

3414ff5875da50edb0fe946ca9cb12f141ea4f81
Doshchinsky <akter1997@gmail.com>
2020-04-02 19:10:35 +0700

    Finished my own version

--- Sem-2/Lab-1/src/readWrite_v2.py
+++ Sem-2/Lab-1/src/readWrite_v2.py
@@ -5,11 +5,11 @@ measuring = 1000				# Amount of measurings in one try
 alphabet = 26					# Length of the given alphabet (Default: Eng - 26 letters)
 
 def genRnd():
-	readersData = [[0 for cnt in range(2)] for cnt in range(experiment - 1)]
-	for attempt in range(1, experiment):
+	readersData = [[0 for cnt in range(experiment)] for cnt in range(2)]
+	for attempt in range(1, experiment + 1):
 		firstReaderAvg = 0		# Average amount of read letters for the first reader's stream
 		secondReaderAvg = 0		# Average amount of read letters for the second reader's stream
-		for cnt in range(1, measuring):
+		for cnt in range(1, measuring + 1):
 			firstReader = 0		# Amount of read letters for the first reader's stream on the current measuring
 			secondReader = 0	# Amount of read letters for the second reader's stream on the current measuring
 			GodBlessRNG = 1		# RND number for deciding when we need to end current measuring
@@ -23,17 +23,22 @@ def genRnd():
 					continue
 			firstReaderAvg += firstReader
 			secondReaderAvg += secondReader
-		readersData[attempt - 1][0] = firstReaderAvg / 1000
-		readersData[attempt - 1][1] = secondReaderAvg / 1000
+		readersData[0][attempt - 1] = firstReaderAvg / 1000
+		readersData[1][attempt - 1] = secondReaderAvg / 1000
 	return readersData
 
-def drawGraph():
-	pass
+def drawGraph(y_arr):
+	x_arr = [cnt for cnt in range(experiment)]			# Array of values similar to alphabet. Made for better graph performance
+	plt.plot(x_arr, y_arr[0], label="First reader's stream")
+	plt.plot(x_arr, y_arr[1], label="Second reader's stream")
+	plt.ylabel("Average amount of read letters")
+	plt.xlabel("Alphabet")
+	plt.legend()
+	plt.show()
 
 def main():
 	data = genRnd()
-	print(data)
-	drawGraph()
+	drawGraph(data)
 
 if __name__ == '__main__':
 	try:

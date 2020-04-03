1a3c230deb2901c59cfaa42a60d9c27c90774cd3
Alexey R <alexey_r.98@ngs.ru>
2020-04-03 23:57:11 +0700

    It's working

--- Sem-2/Lab-2/src/conv_v1.c
+++ Sem-2/Lab-2/src/conv_v1.c
@@ -5,20 +5,29 @@
 int main(int argc, char **argv) {
   int i = 0;
 
+  /* Текущий такт конвейера */
+  long int tact = 1;
+  /* Последнее состояние tact. tact - last_tact = Время нахождения элемента в
+   * конвейере */
+  long int last_tact = tact;
+
   /* Границы времени выполнения этапа конвейера */
-  int min_time = 4;
-  int max_time = 5;
+  int min_time = 1;
+  int max_time = 6;
 
   /* Количество этапов конвейра */
   int pipeline_length = argv[1] ? atoi(argv[1]) : 5;
   if (pipeline_length <= 0) return 253;
 
   /* Количество элементов для обработки */
-  long int items_count = 5;
+  long int items_count = 3;
   /* Оставшиеся элементы */
   long int items_left = items_count;
   /* Обработанные элементы */
   long int items_complete = 0;
+
+  /* Последнее состояние items_complete */
+  long int last_items_complete = 0;
 
   /* Время самого медленного этапа конвейера */
   long int slowly_pipeline_step_time = 0;
@@ -46,7 +55,7 @@ int main(int argc, char **argv) {
   pipeline_state_t *pipeline_status = NULL;
 
   if (!pipeline_times) return 255;
-  srand(time(0));
+  // srand(time(0));
   printf("Times:\n");
   for (i = 0; i < pipeline_length; ++i) {
     pipeline_times[i] = rand() % (max_time - min_time) + min_time;
@@ -68,6 +77,45 @@ int main(int argc, char **argv) {
       slowly_pipeline_step_time * (pipeline_length + items_count - 1);
   printf("Sync pipeline time: %ld\n", sync_pipeline_time);
 
+  /*
+   * Паттерн времен. 5-ти ступенчатый конвейер с последовательным выполнением
+   * этапов.
+   * 5 4 3 2 1
+   * a    b   c  d e|
+   * -------------------------------------------------
+   *      a    b   c  d e|
+   * -------------------------------------------------
+   *           a    b   c  d e|
+   *
+   * 5 1 3 4 2
+   * a    bc  d   e |
+   * -------------------------------------------------
+   *      a    bc d    e |
+   * -------------------------------------------------
+   *
+   * 5 1 5 4 2
+   * -------------------------------------------------
+   * a    bc    d   e |
+   * -------------------------------------------------
+   *      a    bc    d   e |
+   *
+   * 1 2 5 4 2
+   * -------------------------------------------------
+   * ab c    d   e |
+   * -------------------------------------------------
+   *  a b    c    d   e |
+   * -------------------------------------------------
+   *    a    b    c    d   e |
+   *
+   * 5 5 5 5 5 - синхронный конвейер. -> максимальная разница времени выполнения
+   * блоков данных не может быть больше максимального времени выполнения
+   * блока.
+   * -------------------------------------------------
+   * a    b    c    d    e    |
+   * -------------------------------------------------
+   *      a    b    c    d    e    |
+   */
+
   /* Расчет времени выполнения асинхронного конвейера */
   pipeline_status =
       (pipeline_state_t *)malloc(pipeline_length * sizeof(pipeline_state_t));
@@ -78,21 +126,36 @@ int main(int argc, char **argv) {
     pipeline_status[i].work_time = 0;
   }
 
-  /* Цикл пока есть элементы */
-  while (items_complete ^ items_count) {
+  /* Бесконечный цикл */
+  while (1) {
+    /* Если изменилось количество обработанных элементов */
+    if (last_items_complete ^ items_complete) {
+      /*
+       * Эмпирический расчет времени выполнения асинхронного конвейера на основе
+       * моделирования
+       */
+      async_pipeline_time += tact - last_tact;
+      last_tact = tact;
+      last_items_complete = items_complete;
+    }
+    /* Условие выхода из цикла - все элементы обработаны */
+    if (!(items_complete ^ items_count)) break;
+
     if (pipeline_length == 1) {
       /* Одноэтапный конвейер */
       if (pipeline_status[0].ttl <= 0 && !pipeline_status[0].state &&
           items_left) {
         --items_left;
-        pipeline_status[0].ttl = pipeline_times[0];
+        /*
+         * TODO: Понять почему именно в первом(Нулевом) этапе
+         * pipeline_times[0] - 1
+         */
+        pipeline_status[0].ttl = pipeline_times[0] - 1;
         pipeline_status[0].state = 1;
       }
       if (pipeline_status[0].ttl <= 0 && pipeline_status[0].state) {
         ++items_complete;
         pipeline_status[0].state = 0;
-        pipeline_status[0].work_time +=
-            pipeline_times[0] - pipeline_status[0].ttl;
       }
     } else {
       /* Многоэтапный конвейер */
@@ -105,7 +168,11 @@ int main(int argc, char **argv) {
           if (pipeline_status[i].ttl <= 0 && !pipeline_status[i].state &&
               items_left) {
             --items_left;
-            pipeline_status[i].ttl = pipeline_times[i];
+            /*
+             * TODO: Понять почему именно в первом(Нулевом) этапе
+             * pipeline_times[i]- 1
+             */
+            pipeline_status[i].ttl = pipeline_times[i] - 1;
             pipeline_status[i].state = 1;
           }
         } else if (i == pipeline_length - 1) {
@@ -116,8 +183,6 @@ int main(int argc, char **argv) {
           if (pipeline_status[i].ttl <= 0 && pipeline_status[i].state) {
             ++items_complete;
             pipeline_status[i].state = 0;
-            pipeline_status[i].work_time +=
-                pipeline_times[i] - pipeline_status[i].ttl;
           }
           /*
            * Если последний этап свободен - проверяет предыдущий, если
@@ -130,8 +195,6 @@ int main(int argc, char **argv) {
               pipeline_status[i - 1].state = 0;
               pipeline_status[i].state = 1;
               pipeline_status[i].ttl = pipeline_times[i];
-              pipeline_status[i - 1].work_time +=
-                  pipeline_times[i - 1] - pipeline_status[i - 1].ttl;
             }
           }
         } else {
@@ -146,9 +209,6 @@ int main(int argc, char **argv) {
               pipeline_status[i - 1].state = 0;
               pipeline_status[i].state = 1;
               pipeline_status[i].ttl = pipeline_times[i];
-              pipeline_status[i - 1].work_time +=
-                  pipeline_times[i - 1] - pipeline_status[i - 1].ttl;
-              ;
             }
           }
         }
@@ -158,12 +218,20 @@ int main(int argc, char **argv) {
     for (i = pipeline_length - 1; i >= 0; --i) {
       --pipeline_status[i].ttl;
     }
+    ++tact;
   }
-  /* Подсчет общего времени работы всех этапов конвейера */
+
+  printf("Practical async pipeline time: %ld\n", async_pipeline_time);
+  /*
+   * Теоретический расчет времени выполнения асинхронного конвейера на основе
+   * аналитики
+   */
+  async_pipeline_time = 0;
   for (i = 0; i < pipeline_length; ++i) {
-    async_pipeline_time += pipeline_status[i].work_time;
+    async_pipeline_time += pipeline_times[i];
   }
-  printf("Async pipeline time: %ld\n", async_pipeline_time);
+  async_pipeline_time += slowly_pipeline_step_time * (items_count - 1);
+  printf("Theoretical async pipeline time: %ld\n", async_pipeline_time);
   diff_pipeline_time = sync_pipeline_time - async_pipeline_time;
   printf("Time diff: %ld\n", diff_pipeline_time);
   return 0;

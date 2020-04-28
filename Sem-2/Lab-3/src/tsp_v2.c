0900670df1745cac67d837d8e70ec3a6c2a3f7f6
Alexey R <alexey_r.98@ngs.ru>
2020-04-28 11:49:35 +0700

    start branch_and_bound

--- Sem-2/Lab-3/src/tsp_v2.c
+++ Sem-2/Lab-3/src/tsp_v2.c
@@ -16,6 +16,9 @@
 #define LOG_FILE 0
 #define PRINT_WAYS 0
 
+#define TRUE 0
+#define FALSE 1
+
 void SWAP(int* x, int* y) {
   int tmp = *x;
   *x = *y;
@@ -52,6 +55,30 @@ long int get_way_len(int** matrix, int* way, int n) {
     sum += matrix[way[i - 1]][way[i]];
   }
   return sum;
+}
+
+int get_row_min(int** matrix, int n, int row, int greater_or_equal_zero) {
+  int i = 0;
+  int min = __INT32_MAX__;
+  for (i = 0; i < n; ++i) {
+    if ((greater_or_equal_zero && matrix[row][i] > 0) ||
+        (!greater_or_equal_zero && matrix[row][i] >= 0)) {
+      if (matrix[row][i] < min) min = matrix[row][i];
+    }
+  }
+  return min;
+}
+
+int get_col_min(int** matrix, int n, int col, int greater_or_equal_zero) {
+  int i = 0;
+  int min = __INT32_MAX__;
+  for (i = 0; i < n; ++i) {
+    if ((greater_or_equal_zero && matrix[i][col] > 0) ||
+        (!greater_or_equal_zero && matrix[i][col] >= 0)) {
+      if (matrix[i][col] < min) min = matrix[i][col];
+    }
+  }
+  return min;
 }
 
 /* Алгоритм Нарайаны для получения следующей перестановки */
@@ -115,7 +142,7 @@ int* brute_force(int** matrix, int n, long int* id) {
   long int min_way_index = 0;
   /* Длина пути */
   long int way_len = 0;
-  long int min_way_len = __INT32_MAX__;
+  long int min_way_len = __INT64_MAX__;
 
   /* Путь */
   int* way = NULL;
@@ -206,7 +233,164 @@ int* brute_force(int** matrix, int n, long int* id) {
 }
 
 /* Метод ветвей и границ */
-int* branch_and_bound() { return NULL; }
+int* branch_and_bound(int** matrix, int size) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int col_min = __INT32_MAX__;
+  int row_min = __INT32_MAX__;
+
+  /* Копируем размер матрицы, потому что он будет изменяться */
+  int n = size;
+  /* Индекс заполненности пути */
+  int ways_index = 0;
+  /* Нижняя граница */
+  int low_border = 0;
+
+  /* Копия исходной матрицы */
+  int** matrix_c = NULL;
+  /* Матрица оценок */
+  int** matrix_temp = NULL;
+  /* Матрица путей src->dst */
+  int** ways = NULL;
+  /* Минимум по строкам */
+  int* alpha = NULL;
+  /* Минимум по столбцам */
+  int* beta = NULL;
+
+  /*
+   * Если размерность матрицы меньше или 1 - ошибка.
+   * Меньше единицы, потому что Хоббит - Туда и Обратно
+   * Если указатель - NULL, тоже.
+   */
+  if (n <= 1 || !matrix) return NULL;
+
+  /* Выделение памяти под матрицу и вектора */
+  matrix_c = (int**)malloc(n * sizeof(int*));
+  matrix_temp = (int**)calloc(n, sizeof(int*));
+  ways = (int**)calloc(n, sizeof(int*));
+
+  alpha = (int*)malloc(n * sizeof(int));
+  beta = (int*)malloc(n * sizeof(int));
+
+  /* Не выделилась память */
+  if (!matrix_c || !alpha || !beta || !matrix_temp || !ways) return NULL;
+
+  for (i = 0; i < n; ++i) {
+    matrix_c[i] = (int*)malloc(n * sizeof(int));
+    /* Не выделилась память */
+    if (!matrix_c[i]) {
+      for (j = 0; j < i; ++j) free(matrix_c[j]);
+      free(matrix_c);
+      return NULL;
+    }
+  }
+
+  for (i = 0; i < n; ++i) {
+    matrix_temp[i] = (int*)calloc(n, sizeof(int));
+    /* Не выделилась память */
+    if (!matrix_temp[i]) {
+      for (j = 0; j < i; ++j) free(matrix_temp[j]);
+      free(matrix_temp);
+      return NULL;
+    }
+  }
+
+  for (i = 0; i < n; ++i) {
+    ways[i] = (int*)calloc(2, sizeof(int));
+    /* Не выделилась память */
+    if (!ways[i]) {
+      for (j = 0; j < i; ++j) free(ways[j]);
+      free(ways);
+      return NULL;
+    }
+  }
+
+  /* Копируем матрицу. Из-за специфики хранения - построчно */
+  for (i = 0; i < n; ++i) {
+    memcpy(matrix_c[i], matrix[i], n * sizeof(int));
+  }
+
+  low_border = 0;
+  /* Массив минимальных элементов в строке */
+  for (i = 0; i < n; ++i) {
+    alpha[i] = get_row_min(matrix_c, n, i, 0);
+  }
+
+  /* Вычитаем минимальный элемент строки из каждой строки */
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (j != i) matrix_c[i][j] = matrix_c[i][j] - alpha[i];
+    }
+  }
+
+  /* Массив минимальных элементов в столбце */
+  for (i = 0; i < n; ++i) {
+    beta[i] = get_col_min(matrix_c, n, i, 0);
+  }
+
+  /* Вычитаем минимальный элемент столбца из каждого столбца */
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (j != i) matrix_c[i][j] = matrix_c[i][j] - beta[j];
+    }
+  }
+
+  /* Считаем нижнюю границу */
+  for (i = 0; i < n; ++i) low_border = low_border + alpha[i] + beta[i];
+  /* Строим матрицу оценок */
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (matrix_c[i][j] == 0) {
+        for (k = 0; k < n; ++k) {
+          if ((j != k) && (matrix_c[i][k] >= 0) && (matrix_c[i][k] < row_min))
+            row_min = matrix_c[i][k];
+        }
+        for (k = 0; k < n; ++k) {
+          if ((i != k) && (matrix_c[k][j] >= 0) && (matrix_c[k][j] < col_min))
+            col_min = matrix_c[k][j];
+        }
+        matrix_temp[i][j] = row_min + col_min;
+      } else {
+        matrix_temp[i][j] = 0;
+      }
+    }
+  }
+
+  /* Печать матрицы, alpha, beta, lb */
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) fprintf(stderr, "%d\t", matrix_c[i][j]);
+    fprintf(stderr, "\n");
+  }
+  fprintf(stderr, "\n");
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) fprintf(stderr, "%d\t", matrix_temp[i][j]);
+    fprintf(stderr, "\n");
+  }
+  fprintf(logger, "alpha [ ");
+  for (i = 0; i < n; ++i) {
+    fprintf(logger, "%d ", alpha[i]);
+  }
+  fprintf(stderr, "]\n");
+  fprintf(logger, "beta [ ");
+  for (i = 0; i < n; ++i) {
+    fprintf(logger, "%d ", beta[i]);
+  }
+  fprintf(logger, "]\n");
+  fprintf(logger, "lb : %d\n", low_border);
+
+  free(alpha);
+  free(beta);
+  for (i = 0; i < n; ++i) {
+    free(matrix_c[i]);
+  }
+  free(matrix_c);
+  for (i = 0; i < n; ++i) {
+    free(matrix_temp[i]);
+  }
+  free(matrix_temp);
+  return NULL;
+}
 
 int main(int argc, char** argv) {
   int i = 0;
@@ -267,7 +451,7 @@ int main(int argc, char** argv) {
 
   /* Печать матрицы */
   for (i = 0; i < n; ++i) {
-    for (j = 0; j < n; ++j) fprintf(stderr, "%d ", matrix[i][j]);
+    for (j = 0; j < n; ++j) fprintf(stderr, "%d\t", matrix[i][j]);
     fprintf(stderr, "\n");
   }
   time = wtime();
@@ -286,5 +470,12 @@ int main(int argc, char** argv) {
   fprintf(stderr, "\n");
   fprintf(stderr, "Brute time : %lf\n", time);
   free(brute_way);
+
+  branch_and_bound(matrix, n);
+
+  for (i = 0; i < n; ++i) {
+    free(matrix[i]);
+  }
+  free(matrix);
   return 0;
 }

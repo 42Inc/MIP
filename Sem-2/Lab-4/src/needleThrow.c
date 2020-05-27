ed0022d8aca0b7ee3ac4411b8743f0695028bce6
Alexey R <alexey_r.98@ngs.ru>
2020-05-27 12:28:13 +0700

    lab4

--- Sem-2/Lab-4/src/needleThrow.c
+++ Sem-2/Lab-4/src/needleThrow.c
@@ -1,46 +1,45 @@
+#include <inttypes.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <inttypes.h>
-#include <math.h>
 #include <time.h>
 
 const double PI = 3.14159265358979323846;
-const int n = 10000000;
+const int n = 100000;
 
 double wtime() {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
 }
 
-double getrand() {
-  return (double)rand() / RAND_MAX;
-}
-
-double func(double x, double y) {
-  return 3 * pow(y, 2) * pow(sin(x), 2);
-}
+double getrand() { return (double)rand() / RAND_MAX; }
 
 int main(int argc, char **argv) {
   int in = 0;
+  int i = 0;
   double s = 0;
+  double distance = 4;
+  double length = 2;
+
   double t = wtime();
 
-  for (int i = 0; i < n; i++) {
-    double x = getrand() * PI;
-    double y = getrand();
-    if (y <= sin(x)) {
-      in++;
-      s += func(x, y);
+  for (i = 0; i < n; ++i) {
+    double x = getrand() * 2 * distance;
+    double alpha = getrand();
+    if (length * sin(alpha) < x) {
+      ++in;
     }
   }
 
   t = wtime() - t;
-  double v = PI * in / n;
-  double res = v * s / in;
+  double theor = 2 * length / (distance * PI);
+  double pract = (double)(in) / n;
+  double res = fabs(theor - pract);
 
-  printf("Result: %.12f, n %d\ntime = %.6lf\n", res, n, t);
+  printf("Result: %.12f, n: %d\ntime = %.6lf\n", res, n,
+         t);
   return EXIT_SUCCESS;
 }
